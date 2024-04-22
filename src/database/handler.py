0c98a5db1551fae6eb712cc35238b71b9f5b5236
t-nician <dev@senpai.zone>
2024-04-22 09:44:19 -0400

    Account creation, vault decryption & password updating done!

--- src/database/handler.py
+++ src/database/handler.py
@@ -9,13 +9,69 @@ class AccountHandler:
         
         self.account_username = account_model.username
         self.account_email = account_model.email
+        self.account_scrypt = {}
+        self.account_vault = {}
 
         if initialize_account and account_password:
             self.__initialize_account(account_password)
+        else:
+            self.account_scrypt = json.loads(self.account_model.scrypt)
+            self.account_vault = json.loads(self.account_model.vault)
     
 
-    def decrypt_vault(password: str) -> dict:
-        pass
+    def update_password(self, original_password: str, new_password: str):
+        original_hashes, original_salt = secure.hash_password(
+            password=original_password,
+            hashes=2,
+            salt=bytes.fromhex(self.account_scrypt.get("salt"))
+        )
+
+        if original_hashes[0] == bytes.fromhex(self.account_model.password):
+            decrypted_vault_data = secure.decrypt_data(
+                key=original_hashes[1],
+                nonce=bytes.fromhex(self.account_vault.get("nonce")),
+                data=bytes.fromhex(self.account_vault.get("data"))
+            )
+            
+
+            new_hashes, new_salt = secure.hash_password(
+                password=new_password,
+                hashes=2
+            )
+
+            encrypted_vault_data, new_nonce = secure.encrypt_data(
+                key=new_hashes[1],
+                data=decrypted_vault_data
+            )
+
+            self.account_model.password = new_hashes[0].hex()
+
+            self.account_scrypt["salt"] = new_salt.hex()
+
+            self.account_vault["data"] = encrypted_vault_data.hex()
+            self.account_vault["nonce"] = new_nonce.hex()
+
+            self.account_model.scrypt = json.dumps(self.account_scrypt)
+            self.account_model.vault = json.dumps(self.account_vault)
+
+            self.account_model.save()
+
+
+    def decrypt_vault(self, password: str) -> dict:
+        hashes, _ = secure.hash_password(
+            password=password, 
+            hashes=2, 
+            salt=bytes.fromhex(self.account_scrypt.get("salt"))
+        )
+
+        if hashes[0] == bytes.fromhex(self.account_model.password):
+            return json.loads(
+                secure.decrypt_data(
+                    key=hashes[1],
+                    nonce=bytes.fromhex(self.account_vault.get("nonce")),
+                    data=bytes.fromhex(self.account_vault.get("data"))
+                )
+            )
 
 
     def __initialize_account(self, password: str):
--- src/main.py
+++ src/main.py
@@ -6,5 +6,3 @@ account, message = database.try_get_account("username")
 if not account:
     account, message = database.try_create_account("username", "password")
 
-
-print("go")
