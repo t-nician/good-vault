5da83b56b8e5a3ffcba904c7a131ec7eaa4c4bfc
t-nician <dev@senpai.zone>
2024-04-25 17:27:23 -0400

    Clean, rewrite make it bulky but make it simple!

--- src/main.py
+++ src/main.py
@@ -1,11 +1,3 @@
-import data
+import gvault
 
-account = data.object.AccountObject()
-account.update_password("password", "password")
 
-target_data = b'Hello world!'
-
-entry = account.vault.create_private_entry(account.scrypt.get_encryption_key("password"), "test", "test", target_data)
-
-print(entry.data)
-print(entry.get_decrypted_data(account.scrypt.get_encryption_key("password")))
