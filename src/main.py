ca99297d04ce3cbf685c733315385f9743ad62dc
t-nician <dev@senpai.zone>
2024-04-25 20:30:49 -0400

    Cleanup, time to merge!

--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ import gvault
 public_entry = gvault.data.entry.PublicEntry(
     name="Hello",
     note="World",
-    data=gvault.data.entry.FileEntryData(
-        name="file.txt",
-        data=b"file data"
+    data=gvault.data.entry.AccountEntryData(
+        username="username",
+        password="password",
+        website="website"
     )
 )
 
@@ -20,7 +21,9 @@ entry_data = private_entry.to_entry_data(b"password12345678")
 
 if entry_data.type == gvault.data.entry.EntryDataType.ACCOUNT:
     print(entry_data.username, entry_data.password, entry_data.website)
+    
 elif entry_data.type == gvault.data.entry.EntryDataType.FILE:
     print(entry_data.name, entry_data.data)
+    
 elif entry_data.type == gvault.data.entry.EntryDataType.NOTE:
     print(entry_data.note)
