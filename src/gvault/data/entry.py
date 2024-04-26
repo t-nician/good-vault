b6551c97a399dfbc73770df70cef7f5324c183f5
t-nician <dev@senpai.zone>
2024-04-25 20:00:53 -0400

    I'm tryin to cook something up.

--- src/gvault/data/entry.py
+++ src/gvault/data/entry.py
@@ -1,85 +1,53 @@
-import enum, json
-
-from Crypto.Cipher import AES
+import json, enum
 
 
-class EntryDataType(enum.StrEnum):
+class DataEntryType(enum.StrEnum):
     ACCOUNT = "ACCOUNT"
-    NOTE = "NOTE"
     FILE = "FILE"
+    NOTE = "NOTE"
 
 
 class BaseEntryData:
-    def __get_prepped_data_for_encryption(self) -> bytes:
+    def to_bytes(self) -> bytes:
         return b''
-    
-    
-    def __call__(self) -> bytes:
-        return self.__get_prepped_data_for_encryption()
 
 
-class AccountEntryData(BaseEntryData):
+class AccountEntryData:
     def __init__(self, username: str, password: str, website: str | None = ""):
         self.username = username
         self.password = password
         self.website = website
+        
+        self.type = DataEntryType.ACCOUNT
+
     
-    
-    def __get_prepped_data_for_encryption(self) -> bytes:
+    def to_bytes(self) -> bytes:
         return json.dumps({
             "username": self.username,
             "password": self.password,
             "website": self.website
-        }).encode()
-    
-
-class NoteEntryData(BaseEntryData):
-    def __init__(self, note: str):
-        self.note = note
-        
-    
-    def __get_prepped_data_for_encryption(self) -> bytes:
-        return self.note.encode()
+        })
 
 
-class FileEntryData(BaseEntryData):
+class FileEntryData:
     def __init__(self, data: bytes):
         self.data = data
         
+        self.type = DataEntryType.FILE
     
-    def __get_prepped_data_for_encryption(self) -> bytes:
+    
+    def to_bytes(self) -> bytes:
         return self.data
-        
 
-class PrivateEntry:
-    def __init__(self, name: str, type: EntryDataType, nonce: bytes, data: bytes, note: str | None = ""):
-        self.name = name
-        self.type = type
+
+class NoteEntryData:
+    def __init__(self, note: str):
         self.note = note
         
-        self.nonce = nonce
-        self.data = data        
-
-
-class PublicEntry:
-    def __init__(self, name: str, type: EntryDataType, data: BaseEntryData, note: str| None = ""):
-        self.name = name
-        self.note = note
-        self.type = type
-        self.data = data
+        self.type = DataEntryType.NOTE
     
     
-    def create_encrypted_entry(self, encryption_key: bytes) -> PrivateEntry:
-        _data = self.data()
-        _cipher = AES.new(
-            key=encryption_key,
-            mode=AES.MODE_EAX
-        )
-        
-        return PrivateEntry(
-            name=self.name,
-            note=self.note,
-            type=self.type,
-            nonce=_cipher.nonce,
-            data=_cipher.encrypt(_data)
-        )
+    def to_bytes(self) -> bytes:
+        return self.note.encode()
+
+
