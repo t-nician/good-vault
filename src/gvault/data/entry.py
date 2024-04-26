bf930c2fd2eee69f2e6a994b7f402f3345e9e8c6
t-nician <dev@senpai.zone>
2024-04-25 20:14:48 -0400

    We are COOKING

--- src/gvault/data/entry.py
+++ src/gvault/data/entry.py
@@ -1,5 +1,7 @@
 import json, enum
 
+from Crypto.Cipher import AES
+
 
 class DataEntryType(enum.StrEnum):
     ACCOUNT = "ACCOUNT"
@@ -8,6 +10,9 @@ class DataEntryType(enum.StrEnum):
 
 
 class BaseEntryData:
+    def __init__(self):
+        self.type: DataEntryType
+        
     def to_bytes(self) -> bytes:
         return b''
 
@@ -26,7 +31,7 @@ class AccountEntryData:
             "username": self.username,
             "password": self.password,
             "website": self.website
-        })
+        }).encode()
 
 
 class FileEntryData:
@@ -51,3 +56,51 @@ class NoteEntryData:
         return self.note.encode()
 
 
+class PrivateEntry:
+    def __init__(self, name: str, type: DataEntryType, note: str, nonce: bytes, data: bytes):
+        self.name = name
+        self.note = note
+        self.type = type
+        
+        self.nonce = nonce
+        self.data = data
+    
+    
+    def to_entry_data(self, decryption_key: bytes) -> BaseEntryData:
+        _data = AES.new(
+            key=decryption_key, 
+            nonce=self.nonce, 
+            mode=AES.MODE_EAX
+        ).decrypt(self.data)
+        
+        
+        if self.type == DataEntryType.ACCOUNT:
+            _data = json.loads(_data)
+            
+            return AccountEntryData(
+                username=_data["username"],
+                password=_data["password"],
+                website=_data["website"]
+            )
+
+
+class PublicEntry:
+    def __init__(self, name: str, note: str, data: BaseEntryData):
+        self.name = name
+        self.note = note
+        self.data = data
+    
+    
+    def to_private_entry(self, encryption_key: bytes) -> PrivateEntry:
+        _data = self.data.to_bytes()
+        _cipher = AES.new(key=encryption_key, mode=AES.MODE_EAX)
+        
+        return PrivateEntry(
+            name=self.name,
+            type=self.data.type,
+            note=self.note,
+            nonce=_cipher.nonce,
+            data=_cipher.encrypt(_data)
+        )
+        
+        
--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
 import gvault
 
 
+public_entry = gvault.data.entry.PublicEntry(
+    name="Hello",
+    note="World",
+    data=gvault.data.entry.AccountEntryData(
+        username="username",
+        password="password",
+        website="N/A"
+    )
+)
+
+private_entry = public_entry.to_private_entry(b'password12345678')
+
+print(private_entry.to_entry_data(b'password12345678').username)
