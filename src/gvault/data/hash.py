ade24e7eea8afb11464c991bdb4f7ec9013d09f1
t-nician <dev@senpai.zone>
2024-04-27 16:57:01 -0400

    This will be used later, gotta comment for reminder!

--- src/gvault/data/hash.py
+++ src/gvault/data/hash.py
@@ -26,6 +26,26 @@ class ScryptHashData:
         return self.__scrypt(password, 1)
     
     
+    def get_encryption_key(self, password: str) -> bytes:
+        return self.__scrypt(password, 2)[1]
+    
+    
+    def get_authorization_and_encryption_key(
+        self, password: str
+    ) -> list[bytes]:
+        """Hashes the password and returns two keys.
+
+        Args:
+            password (str): Password for the hashes to generate from.
+
+        Returns: list[bytes]
+            * list[bytes][0] is authorization key.
+                        
+            * list[bytes][1] is encryption key.
+        """
+        return self.__scrypt(password, 2)
+    
+    
     def __scrypt(
         self, password: str, num_keys: int | None = 1
     ) -> bytes | list[bytes]:
