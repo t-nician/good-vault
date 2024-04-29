3af9887fcd5b261b402be183ee8ff20d9fc2ba9b
t-nician <dev@senpai.zone>
2024-04-28 21:49:59 -0400

    We cook.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 import gvault
 
+from Crypto.Random import get_random_bytes
+
+encrypted_entry = gvault.data.entry.EncryptedEntryData(
+    cipher_nonce=get_random_bytes(12),
+    encrypted_data=b"encrypted data"
+)
+
+print(encrypted_entry.to_dict(True))
