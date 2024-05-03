7017b67a47227e6a9c92cf38dd393c7401bba212
t-nician <dev@senpai.zone>
2024-05-03 18:13:08 -0400

    Rewrite time!

--- src/main.py
+++ src/main.py
@@ -1,13 +1,2 @@
 import gvault
 
-vault_item = gvault.data.VaultEntry(
-    entry_data=gvault.data.AccountEntry(
-        username="Username",
-        password="Password",
-        website="Website.com"
-    )
-)
-
-vault_item.encrypt(b"password12345678")
-
-print(vault_item.entry_data)
