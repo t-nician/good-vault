743f5529dc18df3b35ffd96933fd650f03fef3a0
t-nician <dev@senpai.zone>
2024-05-03 14:54:35 -0400

    RE-RE-RE-RE-RE-REWRITE TIME!

--- src/gvault/data.py
+++ src/gvault/data.py
@@ -5,6 +5,8 @@ import dataclasses
 
 from gvault import base
 
+from Crypto.Cipher import AES
+
 
 @dataclasses.dataclass
 class BaseItemData(base.BaseFunctions):
@@ -27,13 +29,80 @@ class NoteItemData(BaseItemData):
 
 
 @dataclasses.dataclass
+class EncryptedItemField(BaseItemData):
+    name: str = base.data_field(save=True, encrypt=False, default="")
+    
+    data: bytes = base.data_field(save=True, encrypt=False, default=b"")
+    nonce: bytes = base.data_field(save=True, encrypt=False, default=b"")
+    
+
+@dataclasses.dataclass
+class EncryptedItemData(BaseItemData):
+    encrypted_item_fields: list[EncryptedItemField] = base.data_field(
+        save=True, encrypt=False, default_factory=list
+    )
+
+    decrypted_item_data: AccountItemData | NoteItemData = base.data_field(
+        save=True, encrypt=False, default_factory=BaseItemData
+    )
+
+
+@dataclasses.dataclass
 class VaultItem(base.BaseFunctions):
     name: str = base.data_field(save=True, encrypt=False, default="")
     note: str = base.data_field(save=True, encrypt=False, default="")
     
-    item_data: BaseItemData = base.data_field(
+    item_data: EncryptedItemData | AccountItemData | NoteItemData = base.data_field(
         save=True, encrypt=False, default_factory=BaseItemData
     )
+    
+    __is_encrypted: bool = base.data_field(
+        save=False, encrypt=False, default=False
+    )
+    
+    def __post_init__(self):
+        if type(self.item_data) is EncryptedItemData:
+            self.__is_encrypted = True
+    
+    
+    def encrypt_item(self, key: bytes):
+        if self.__is_encrypted:
+            return None
+
+        encrypted_item_fields = []
+        
+        for field in dataclasses.fields(self.item_data):
+            if field.metadata["save"] and field.metadata["encrypt"]:
+                
+                data = getattr(self.item_data, field.name, "")
+                data_type = type(data)
+                
+                if data_type is str:
+                    data = data.encode()
+                elif data_type is list or data_type is dict:
+                    data = json.dumps(data).encode()
+                elif callable(getattr(data, "to_json", None)):
+                    data = data.to_json(bytes_to_hex=False).encode()
+
+                cipher = AES.new(key=key, mode=AES.MODE_EAX)
+                nonce = cipher.nonce
+                
+                encrypted_data = cipher.encrypt(data)
+                
+                encrypted_field = EncryptedItemField(
+                    name=field.name,
+                    data=encrypted_data,
+                    nonce=nonce
+                )
+                
+                encrypted_item_fields.append(encrypted_field)
+            elif field.metadata["save"]:
+                
+                
+    
+    
+    def decrypt_item(self, key: bytes):
+        pass
     
 
 @dataclasses.dataclass
@@ -41,5 +110,7 @@ class VaultData(base.BaseFunctions):
     vault_items: list[VaultItem] = base.data_field(
         save=True, encrypt=False, default_factory=list
     )
+    
+    
     
     
--- src/main.py
+++ src/main.py
@@ -3,3 +3,15 @@ import dataclasses
 
 import gvault
 
+vault_item = gvault.data.VaultItem(
+    name="name",
+    note="note",
+    item_data=gvault.data.AccountItemData(
+        username="username",
+        password="password",
+        website="website"
+    )
+)
+
+
+vault_item.encrypt_item(b"password12345678")
