09771ffe15fc17fb456b3d9c8ac9b73074743144
t-nician <dev@senpai.zone>
2024-05-03 14:55:00 -0400

    LET ME COOK PLSSSS

--- src/main.py
+++ src/main.py
@@ -1,17 +1,5 @@
+import gvault
+
 import json
 import dataclasses
 
-import gvault
-
-vault_item = gvault.data.VaultItem(
-    name="name",
-    note="note",
-    item_data=gvault.data.AccountItemData(
-        username="username",
-        password="password",
-        website="website"
-    )
-)
-
-
-vault_item.encrypt_item(b"password12345678")
