7646ff860992027284bb0c981588cfa6f06760fa
t-nician <dev@senpai.zone>
2024-05-03 20:49:56 -0400

    Latest iteration of standarizing data input for the vault.

--- src/gvault/data.py
+++ src/gvault/data.py
@@ -5,3 +5,66 @@ import dataclasses
 
 from Crypto.Cipher import AES
 
+
+@dataclasses.dataclass
+class FieldData:
+    uuid: str = dataclasses.field(default_factory=lambda: uuid.uuid4().hex)
+    
+    name: str = dataclasses.field(default="")
+    note: str = dataclasses.field(default="")
+
+    nonce: bytes = dataclasses.field(default=b"")    
+    data: str | bytes = dataclasses.field(default="")
+
+    encrypted: bool = dataclasses.field(default=False)
+    do_i_encrypt: bool = dataclasses.field(default=False)
+    
+    def __post_init__(self):
+        if self.nonce != b"":
+            if type(self.data) is bytes:
+                self.encrypted = True
+            else:
+                raise Exception("FieldData has nonce but data is not bytes?")
+
+
+@dataclasses.dataclass
+class ItemData:
+    name: str = dataclasses.field(default="")
+    note: str = dataclasses.field(default="")
+    
+    fields: list[FieldData] = dataclasses.field(default_factory=list)
+    private_field_uuids: list[str] = dataclasses.field(default_factory=list)
+
+    def add_field(
+        self, name: str, note: str, data: str | bytes, 
+        do_i_encrypt: bool | None = False,
+        key: bytes | None = None,
+    ) -> FieldData:
+        if do_i_encrypt and key:
+            cipher = AES.new(key=key, mode=AES.MODE_EAX)
+            
+            new_field = FieldData(
+                name=name,
+                note=note,
+                data=cipher.encrypt(
+                    type(data) is str and data.encode() or data
+                ),
+                nonce=cipher.nonce,
+                do_i_encrypt=do_i_encrypt
+            )
+            
+            self.private_field_uuids.append(new_field.uuid)
+            self.fields.append(new_field)
+            
+            return new_field
+
+        else:
+            new_field = FieldData(
+                name=name,
+                note=note,
+                data=data
+            )
+            
+            self.fields.append(new_field)
+            
+            return new_field
--- src/main.py
+++ src/main.py
@@ -1,2 +1,23 @@
 import gvault
 
+item_data = gvault.data.ItemData(
+    name="name",
+    note="note"
+)
+
+username_field = item_data.add_field(
+    name="username",
+    note="username-note",
+    data="Epic_Username123"
+)
+
+password_field = item_data.add_field(
+    name="password",
+    note="password-note",
+    data="Epic_Password123",
+    
+    do_i_encrypt=True,
+    key=b"password12345678"
+)
+
+print(password_field)
