fa59ade38ebbc83964676a34491086bfacc17db3
t-nician <dev@senpai.zone>
2024-05-11 15:28:31 -0400

    redo again i wanna make this CLEAN

--- src/gvault/data.py
+++ src/gvault/data.py
@@ -4,9 +4,3 @@ import dataclasses
 from gvault import util
 from Crypto.Cipher import AES
 
-@util.vaultdata
-class AccountData:
-    website = util.Field(save=True)
-    username = util.Field(save=True)
-
-    password = util.Field(save=True, encrypt=True)
--- src/main.py
+++
@@ -1,8 +0,0 @@
-import gvault
-import dataclasses
-
-@gvault.util.vaultdata
-class TestData:
-    test_field = gvault.util.Field(save=True, encrypt=True)
-
-
