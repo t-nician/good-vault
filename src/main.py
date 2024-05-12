1d4b706b48dbfea094ddb9045e03c11dfad0d71a
t-nician <dev@senpai.zone>
2024-05-12 12:43:05 -0400

    MODIFICATIONS :D

--- src/main.py
+++ src/main.py
@@ -1,24 +1,2 @@
 import gvault
 
-vault = gvault.VaultManager()
-
-vault.add_data_entry_from_dict({
-    "type": "account",
-
-    "website": {
-        "save": True, "encrypt": False, "is_encrypted": False,
-        "data": "website.com", "nonce": "N/A"
-    },
-
-    "username": {
-        "save": True, "encrypt": False, "is_encrypted": False,
-        "data": "username", "nonce": "N/A"
-    },
-
-    "password": {
-        "save": True, "encrypt": True, "is_encrypted": False,
-        "data": "password123", "nonce": "N/A"
-    }
-})
-
-print(vault.data_entries[0].wrap.username)
