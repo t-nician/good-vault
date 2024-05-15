ff4bdc19825cecacdece785e7394cf6fc8a79730
t-nician <dev@senpai.zone>
2024-05-15 10:37:38 -0400

    Rewrite again, code icky we fixy!

--- src/gvault/core/__init__.py
+++
@@ -1,2 +0,0 @@
-from gvault.core import controller, entry, tool
-
--- src/gvault/core/tool.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-available_entry_classes: dict[str, BaseModel] = {}
-
-
-def add_entry_class(entry_class: object):
-    available_entry_classes[entry_class.__name__] = entry_class
-    return entry_class
